260d1b1c768a4b1281091d190341cb46eaba844c
Jacudibu <jacudibu@gmail.com>
2018-02-22 18:56:51 +0100

    Added Vertice Generation for the upcoming Mesh Generator

--- Assets/Scripts/WorldGeneration/HeightmapConverter.cs
+++ Assets/Scripts/WorldGeneration/HeightmapConverter.cs
@@ -4,6 +4,9 @@ namespace WorldGeneration
 {
     public static class HeightmapConverter
     {
+        private const float MaxHeight = 50f;
+        private const float DistanceBetweenVertices = 0.1f;
+        
         public static Texture2D ToTexture2D(Heightmap heightmap)
         {
             var texture = new Texture2D(heightmap.Width, heightmap.Height, TextureFormat.ARGB32, false);
@@ -27,7 +30,42 @@ namespace WorldGeneration
         {
             Mesh mesh = new Mesh();
 
+            mesh.vertices = GenerateVertices(heightmap);
+            Vector3[] uvs;
+            Vector3[] tris;
+
+            
             return mesh;
+        }
+
+        private static Vector3[] GenerateVertices(Heightmap heightmap)
+        {
+            var vertices = new Vector3[heightmap.Values.Length];
+
+            int i = 0;
+            for (int y = 0; y < heightmap.Height; y++)
+            {
+                for (int x = 0; x < heightmap.Width; x++)
+                {
+                    float value = heightmap.GetAt(x, y);
+                    var vertice = new Vector3(x * DistanceBetweenVertices, 
+                                              y * DistanceBetweenVertices,
+                                              heightmap.GetAt(x, y) * MaxHeight);
+
+                    vertices[i] = vertice;
+                    i++;
+                }
+            }
+
+            return vertices;
+        }
+
+        private static int[] GenerateTriangles(Heightmap heightmap)
+        {
+            int[] tris = new int[425];
+            
+            
+            return tris;
         }
     }
 }
